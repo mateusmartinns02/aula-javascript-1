c0408c31e008b73195dfd6b93ccc6d30c6fa371e
luizz0705 <luizzhenrique0705@gmail.com>
2024-10-02 21:01:31 -0300

    lista de frutas

---
+++ frutas.js
@@ -0,0 +1,6 @@
+const frutas = ["maca", "manga", "morango", "melancia", "maracuja", "acerola"];
+
+console.log("A fruta na posição 1 é: ", frutas[1]);
+console.log("A fruta na posição 4 é: ", frutas[4]);
+console.log("Meu array tem ", frutas.length, "frutas");
+
